e7878ac97d828791eebe31cd91849f7bd3303c9c
kryak_vak <mr.vakashev@mail.ru>
2025-10-21 03:14:39 +0300

    working version; compose.yaml & env variables changed; schema-registry dependancies added; serialize_base_model created to replace str_to_dict

--- orders/src/infra/kafka/registry.py
+++ orders/src/infra/kafka/registry.py
@@ -1,3 +1,5 @@
+from uuid import UUID
+
 from confluent_kafka.schema_registry import SchemaRegistryClient
 from confluent_kafka.schema_registry.json_schema import JSONSerializer
 from confluent_kafka.serialization import (
@@ -11,13 +13,30 @@ from src.infra.kafka.schemas import order_schema_str
 schema_registry_client = SchemaRegistryClient(schema_registry_config)
 
 
-def str_to_dict(dto: BaseModel):
+def str_to_dict(dto: BaseModel, ctx):
     return dto.model_dump()
+
+
+def serialize_base_model(obj: BaseModel, _):
+    def convert(value):
+        if isinstance(value, UUID):
+            return str(value)
+        elif isinstance(value, BaseModel):
+            return serialize_base_model(value, None)
+        elif isinstance(value, list):
+            return [convert(v) for v in value]
+        elif isinstance(value, dict):
+            return {k: convert(v) for k, v in value.items()}
+        return value
+
+    return {k: convert(v) for k, v in obj.model_dump().items()}
 
 
 def creat_json_serializer(schema_str):
     return JSONSerializer(
-        schema_str, schema_registry_client, str_to_dict # type: ignore
+        schema_str, # type: ignore
+        schema_registry_client,
+        serialize_base_model
     )
 
 
